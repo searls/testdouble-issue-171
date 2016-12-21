e44ebd9841adbc72070c4a4d9ab4d9c24f457eda
Patrick Boisen <patrick@skarp.dk>
2016-12-21 12:54:58 +0100

    Was able to trigger the error without the factory

--- test/2_moduleA.spec.js
+++ test/2_moduleA.spec.js
@@ -6,13 +6,11 @@ const td = require('testdouble');
 lab.experiment('moduleA', () => {
     console.log('A');
 
-    let factory;
     let SomeClass;
     let someFunction;
     let moduleA;
 
     lab.beforeEach((done) => {
-        factory = td.replace('../lib/factory');
         someFunction = td.replace('../lib/someFunction');
         SomeClass = require('../lib/SomeClass').SomeClass;
 
@@ -30,11 +28,9 @@ lab.experiment('moduleA', () => {
 
     lab.test('calls someFunction with an instance of SomeClass', (done) => {
 
-        td.when(factory.get()).thenReturn({animal: 'dog'});
-
         moduleA.moduleA();
 
-        td.verify(someFunction.someFunction(td.matchers.anything(), td.matchers.isA(SomeClass)), { times: 1 });
+        td.verify(someFunction.someFunction(td.matchers.isA(SomeClass)), { times: 1 });
 
         done();
     });
